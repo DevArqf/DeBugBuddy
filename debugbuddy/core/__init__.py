7b8a87db7fa206e88cc873e25fe80725342193a3
DevArqf <malik.magicly@gmail.com>
2025-12-23 04:01:18 -0400

    fix: incorrect path causing pytest to fail

--- debugbuddy/core/__init__.py
+++ debugbuddy/core/__init__.py
@@ -1,15 +1,15 @@
 from debugbuddy.core.parsers import ErrorParser
 from debugbuddy.core.explainer import ErrorExplainer
-from debugbuddy.monitoring.watcher import ErrorWatcher
-from debugbuddy.storage.history import HistoryManager
 from debugbuddy.core.predictor import ErrorPredictor
 from debugbuddy.core.trainer import PatternTrainer
+from debugbuddy.monitoring.watcher import ErrorWatcher
+from debugbuddy.storage.history import HistoryManager
 
 __all__ = [
-    'ErrorParser',
-    'ErrorExplainer',
+    "ErrorParser",
+    "ErrorExplainer",
+    "ErrorPredictor",
+    "PatternTrainer",
     'ErrorWatcher',
-    'HistoryManager',
-    'ErrorPredictor',
-    'PatternTrainer',
+    'HistoryManager'
 ]
